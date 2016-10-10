252405f4a0f9bd8206a6474296c715454eeba8c7
Thibaut Othomene <Thibom1326@gmail.com>
2016-10-10 15:35:18 +0200

    Creation et implementation class Message, iframe fixe dans interface.html, correction css mineures

---
+++ src/main/java/JAVA/Message.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JAVA;
+
+/**
+ *
+ * @author thibom
+ */
+public class Message{
+    private String pseudo;
+    private String message;
+    
+    public Message(String pseudo, String message){
+        this.pseudo = pseudo;
+        this.message = message;
+    } 
+    
+    public String getMessage(){
+        return message;
+    }
+}
